00508f33d0d08020641ff7e086fda37087c2ad58
Ye Htun (Henry) <87560504+yehtunaung@users.noreply.github.com>
2024-12-16 10:51:19 +0630

    Update AJAX_check_id.php

--- admin/AJAX_check_id.php
+++ admin/AJAX_check_id.php
@@ -3,11 +3,6 @@
 // key to authenticate
 define('INDEX_AUTH', '1');
 
-/*
-A Handler script for AJAX ID checking
-Arie Nugraha 2007
-*/
-
 require_once '../sysconfig.inc.php';
 // session checking
 require SB.'admin/default/session.inc.php';
